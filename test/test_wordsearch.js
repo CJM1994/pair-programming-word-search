1b45b84ebe56c1b6c62c0469baffa3ea9af2dd6c
CJM1994 <connorjohnmullin@gmail.com>
2022-02-16 20:02:45 -0800

    completed assignment with travis

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,41 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertical", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'L', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'A', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'Y', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is not present vertically", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'L', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'D', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRYSMITH')
+
+    assert.isFalse(result);
+  });
+  it("should return false if the array is empty", function () {
+    const result = wordSearch([], 'LARRY')
+
+    assert.isFalse(result);
   });
 });
